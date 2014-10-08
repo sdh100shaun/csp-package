c055cf92eefc8ff1506081a48d58755ccbb24957
Shaun Hare <shaun@shaunhare.co.uk>
2014-10-08 12:57:25 +0100

    adding in config skeleton and working through ideas

---
+++ ShaunHare/csp/src/ShaunHare/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * @author: Shaun Hare
+ * Date: 08/10/2014
+ * Time: 12:55
+ * 
+ */
+
+namespace workbench\ShaunHare\csp\src\ShaunHare;
+
+
+class Config {
+    
+
+} 
--- ShaunHare/csp/src/ShaunHare/Csp/Composers/CspComposer.php
+++ ShaunHare/csp/src/ShaunHare/Csp/Composers/CspComposer.php
@@ -32,5 +32,6 @@ class CspComposer {
 
 
 
+
     }
 } 
