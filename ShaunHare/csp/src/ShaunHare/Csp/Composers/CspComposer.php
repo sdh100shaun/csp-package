80c50fb2e85177a148d6d7839d2c858bba1770b3
Shaun Hare <shaun@shaunhare.co.uk>
2014-10-13 17:02:53 +0100

    started the scriptsrc and nonce parts

--- ShaunHare/csp/src/ShaunHare/Csp/Composers/CspComposer.php
+++ ShaunHare/csp/src/ShaunHare/Csp/Composers/CspComposer.php
@@ -12,25 +12,52 @@ namespace ShaunHare\Csp\Composers;
 
 class CspComposer {
 
+    private $nonceCreator;
+
+    private $cspPolicy;
+
+
+    public function __construct( NonceCreator $nonceCreator)
+    {
+
+        $this->nonceCreator = $nonceCreator;
+    }
+
     public function compose($view)
     {
-        $view->with('csp-header',header("Content-Security-Policy:".$cspPolicy));
+
+
+        $view->with('csp-header',header("Content-Security-Policy:".$this->cspPolicy));
+
         $view->with("csp-script",$this->makeScriptTag());
     }
+
+
 
     public function makeScriptTag()
     {
 
     }
+
+    private function makeScriptSrc()
+    {
+        $this->cspPolicy["script-src"] = "self";
+        
+        if(isset($this->nonceCreator) && !empty($this->nonceCreator->getNonce()))
+        {
+            $this->cspPolicy["script-src"] .=" nonce-".$this->nonceCreator->getNonce();
+        }
+
+    }
+
     private function makeClosingScriptTag()
     {
         return "</script>";
 
     }
+
     private function makeOpeningScriptTag()
     {
-
-
 
 
     }
